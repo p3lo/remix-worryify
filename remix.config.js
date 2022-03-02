6740aaec58daaa79ea5f9f2abe8845f685fbc0ee
Daniel Slanicka <mysiacik@mysiacik.com>
2022-03-02 22:33:31 +0100

    update for vercel

--- remix.config.js
+++ remix.config.js
@@ -2,10 +2,14 @@
  * @type {import('@remix-run/dev').AppConfig}
  */
 module.exports = {
-  ignoredRouteFiles: [".*"]
+  ignoredRouteFiles: ['.*'],
   // appDirectory: "app",
   // assetsBuildDirectory: "public/build",
   // serverBuildPath: "build/index.js",
   // publicPath: "/build/",
   // devServerPort: 8002
+  appDirectory: 'app',
+  browserBuildDirectory: 'public/build',
+  publicPath: '/build/',
+  serverBuildDirectory: 'api/build',
 };
